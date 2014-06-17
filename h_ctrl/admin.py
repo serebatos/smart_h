40d7f03c0364c0468875086a5ee302dbf8c06de1
unknown <reprintsev_sv@NB0406SL.bdo.ru>
2014-06-17 12:32:44 +0400

    Home-flat-pi structure

--- h_ctrl/admin.py
+++ h_ctrl/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from h_ctrl.models import Pi, Action, Home
+from h_ctrl.models import Pi, Action, Home, Schedule
 # Register your models here.
 
 
 admin.site.register(Home)
 admin.site.register(Pi)
 admin.site.register(Action)
+admin.site.register(Schedule)
 
 
--- h_ctrl/models.py
+++ h_ctrl/models.py
@@ -1,11 +1,15 @@
+from compiler.ast import name
 from django.db import models
 
 # Create your models here.
+from django.db.models.fields.related import ManyToOneRel
 
 
 class Home(models.Model):
     name = models.CharField(max_length=20)
 
+    def __unicode__(self):
+        return self.name
 
 
 class Pi(models.Model):
@@ -17,9 +21,21 @@ class Pi(models.Model):
 
 
 class Action(models.Model):
-    controller = models.ForeignKey(Pi)
+    controller = models.ForeignKey(Pi, rel_class=ManyToOneRel)
     name = models.CharField(max_length=20)
     pin = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Schedule(models.Model):
+    actions = models.ManyToManyField(Action)
+    name = models.CharField(max_length=20)
+    start_time = models.TimeField(name="Start")
+    end_time = models.TimeField(name="End")
+    certain_date = models.DateTimeField(name="One time run date")
+    run_every_days = models.IntegerField(default=1)
 
     def __unicode__(self):
         return self.name
