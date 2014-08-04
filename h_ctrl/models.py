fb6701b4722a5759be008ca5a631ab06d56ad0f0
serebatos <serebatos@gmail.com>
2014-08-04 22:17:49 +0400

    New models

--- h_ctrl/models.py
+++ h_ctrl/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models.fields.related import ManyToManyField, ManyToManyRel
 
 
 class Home(models.Model):
     name = models.CharField(max_length=20)
 
+    def __unicode__(self):
+        return self.name
 
 
 class Pi(models.Model):
@@ -20,6 +23,20 @@ class Action(models.Model):
     controller = models.ForeignKey(Pi)
     name = models.CharField(max_length=20)
     pin = models.IntegerField(default=0)
+    cmd_code=models.CharField(max_length=1)
+
+    def __unicode__(self):
+        return self.name + ":" + self.controller.name
+
+
+class Schedule(models.Model):
+    name = models.CharField(max_length=20)
+    # start_time = models.TimeField("Start time")
+    # duration = models.IntegerField(default=0)
+    # days_repeat=models.IntegerField(default=1) #repeat everyday by default
+    # pi=models.ForeignKey(Pi)
+    action = models.ManyToManyField(Action)
+
 
     def __unicode__(self):
         return self.name
