710985b40b7272dd60b223920c1fee884c2b29e5
serebatos <serebatos@gmail.com>
2014-08-05 23:51:29 +0400

    Schedule fix

--- h_ctrl/models.py
+++ h_ctrl/models.py
@@ -30,17 +30,17 @@ class Action(models.Model):
         return self.name + ":" + self.controller.name
 
 
-class Schedule(models.Model):
-    name = models.CharField(max_length=20)
-    # start_time = models.TimeField("Start time")
-    # duration = models.IntegerField(default=0)
-    # days_repeat=models.IntegerField(default=1) #repeat everyday by default
-    # pi=models.ForeignKey(Pi)
-    action = models.ManyToManyField(Action)
-
-
-    def __unicode__(self):
-        return self.name
+# class Schedule(models.Model):
+#     name = models.CharField(max_length=20)
+#     # start_time = models.TimeField("Start time")
+#     # duration = models.IntegerField(default=0)
+#     # days_repeat=models.IntegerField(default=1) #repeat everyday by default
+#     # pi=models.ForeignKey(Pi)
+#     action = models.ManyToManyField(Action)
+#
+#
+#     def __unicode__(self):
+#         return self.name
 
 
 class Schedule(models.Model):
